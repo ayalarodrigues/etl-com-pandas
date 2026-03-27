a014be5209b6927f528e02f855eb4436a9b8f8bc
Ayala <ayalafreire7@gmail.com>
2026-03-27 08:40:27 -0300

    feat: adiciona função de extração com leitura dos arquivos CSV

--- script_etl_pandas.py
+++ script_etl_pandas.py
@@ -8,14 +8,19 @@ import pandas as pd
 def extracao():
 
     vendas = pd.read_csv("vendas.csv")
-    clientes = pd.read_csv("cliente.csv")
+    clientes = pd.read_csv("clientes.csv")
     produtos = pd.read_csv("produtos.csv")
     base_bruta = pd.read_csv("dados_vendas_etl_pandas_6000_linhas.csv")
 
     print("--- ARQUIVOS LIDOS ---")
 
+    return vendas, clientes, produtos, base_bruta
+
 def main():
-    pass
+
+    vendas, cliete, produtos, base_bruta = extracao()
+    print("\nExtração realizada!\n")
+    
 
 if __name__ == "__main__":
     main()
